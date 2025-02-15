a92cdb4c8a3e578aa90b4c6577956c04ed17b986
Shannon Ware <photius@shannonware.com>
2025-02-15 18:59:55 +0000

    Implement removal of unblur for index page images.

--- static/terzo.js
+++ static/terzo.js
@@ -6,37 +6,45 @@ Replace low res images with high res images.
 window.onload = function loadStuff() {
     console.log("Begin...");
     try {
-        document.getElementById("hero").src="static/images/IMG_20250202_173854_Hero_1024x681.jpg";
+        const target = document.getElementById("hero")
+        target.src="static/images/IMG_20250202_173854_Hero_1024x681.jpg";
+        target.style.filter = "none";
 
     } catch (e) {
         console.log("hero - Caught: " + toString(e))
     }
     
     try {
-        document.getElementById("knoll-one").src="static/images/IMG_20250202_161322_HDR_KNOLL_brand_Knitting_Wool_1024x771.jpg";
+        const target = document.getElementById("knoll-one")
+        target.src="static/images/IMG_20250202_161322_HDR_KNOLL_brand_Knitting_Wool_1024x771.jpg";
+        target.style.filter = "none";
 
     } catch (e) {
-        console.log("hero - Caught: " + toString(e))
+        console.log("knoll - Caught: " + toString(e))
     }
     
     try {
-        document.getElementById("dyneema-one").src="static/images/IMG_20250202_131517_HDR_Dyneema_Merino_Wool_Cones_771x1024.jpg";
+        const target = document.getElementById("dyneema-one")
+        target.src="static/images/IMG_20250202_131517_HDR_Dyneema_Merino_Wool_Cones_771x1024.jpg";
+        target.style.filter = "none";
 
     } catch (e) {
-        console.log("hero - Caught: " + toString(e))
+        console.log("dymeena - Caught: " + toString(e))
     }
     
     try {
-        document.getElementById("hero-two").src="static/images/IMG_20250202_173908_Hero_1024x771.jpg";
+        const target = document.getElementById("hero-two")
+        target.src="static/images/IMG_20250202_173908_Hero_1024x771.jpg";
+        target.style.filter = "none";
 
     } catch (e) {
-        console.log("hero - Caught: " + toString(e))
+        console.log("hero two - Caught: " + toString(e))
     }
     
     try {
         const target = document.getElementById("wool-not-found")
         target.src="static/images/wool-not-found.png";
-        target.style.filter = "none" // "grayscale(100%)";
+        target.style.filter = "none"; // "grayscale(100%)";
 
     } catch (e) {
         console.log("error - Caught: " + toString(e))
