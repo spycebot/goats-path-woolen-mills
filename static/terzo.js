744751840d2e504705a052472cb61b6ea5eced7e
Shannon Ware <photius@shannonware.com>
2025-02-15 18:53:21 +0000

    Change 404 image; blur preload and unblur final image.

--- static/terzo.js
+++ static/terzo.js
@@ -5,9 +5,43 @@ Replace low res images with high res images.
 
 window.onload = function loadStuff() {
     console.log("Begin...");
-    document.getElementById("hero").src="static/images/IMG_20250202_173854_Hero_1024x681.jpg";
-    document.getElementById("knoll-one").src="static/images/IMG_20250202_161322_HDR_KNOLL_brand_Knitting_Wool_1024x771.jpg";
-    document.getElementById("dyneema-one").src="static/images/IMG_20250202_131517_HDR_Dyneema_Merino_Wool_Cones_771x1024.jpg";
-    document.getElementById("hero-two").src="static/images/IMG_20250202_173908_Hero_1024x771.jpg";
+    try {
+        document.getElementById("hero").src="static/images/IMG_20250202_173854_Hero_1024x681.jpg";
+
+    } catch (e) {
+        console.log("hero - Caught: " + toString(e))
+    }
+    
+    try {
+        document.getElementById("knoll-one").src="static/images/IMG_20250202_161322_HDR_KNOLL_brand_Knitting_Wool_1024x771.jpg";
+
+    } catch (e) {
+        console.log("hero - Caught: " + toString(e))
+    }
+    
+    try {
+        document.getElementById("dyneema-one").src="static/images/IMG_20250202_131517_HDR_Dyneema_Merino_Wool_Cones_771x1024.jpg";
+
+    } catch (e) {
+        console.log("hero - Caught: " + toString(e))
+    }
+    
+    try {
+        document.getElementById("hero-two").src="static/images/IMG_20250202_173908_Hero_1024x771.jpg";
+
+    } catch (e) {
+        console.log("hero - Caught: " + toString(e))
+    }
+    
+    try {
+        const target = document.getElementById("wool-not-found")
+        target.src="static/images/wool-not-found.png";
+        target.style.filter = "none" // "grayscale(100%)";
+
+    } catch (e) {
+        console.log("error - Caught: " + toString(e))
+    }
+
+    // NG document.getElementById("hero").innerHTML = "<img src='{{ url_for('static', filename='images/IMG_20250202_173854_Hero_1024x681.jpg') }}' />"
     console.log("End.");
   };
