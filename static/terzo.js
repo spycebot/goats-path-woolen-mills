d0e2012e0a2c45913fb63263bff316896906e212
Shannon Ware <photius@shannonware.com>
2025-02-15 11:38:23 +0000

    Change header one font to Sigmar, add two images to index page

--- static/terzo.js
+++ static/terzo.js
@@ -5,6 +5,9 @@ Replace low res images with high res images.
 
 window.onload = function loadStuff() {
     console.log("Begin...");
-    document.getElementById("hero").src="/static/images/IMG_20250202_173854_Hero_1024x681.jpg"
+    document.getElementById("hero").src="/static/images/IMG_20250202_173854_Hero_1024x681.jpg";
+    document.getElementById("knoll-one").src="/static/images/IMG_20250202_161322_HDR_KNOLL_brand_Knitting_Wool_1024x771.jpg";
+    document.getElementById("dyneema-one").src="/static/images/IMG_20250202_131517_HDR_Dyneema_Merino_Wool_Cones_771x1024.jpg";
+    document.getElementById("hero-two").src="/static/images/IMG_20250202_173908_Hero_1024x771.jpg";
     console.log("End.");
   };
